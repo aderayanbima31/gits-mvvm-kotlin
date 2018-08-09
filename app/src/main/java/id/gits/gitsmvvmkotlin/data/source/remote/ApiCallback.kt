d07b2a709bef78f775ab841246c68911c2604320
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2018-08-09 20:54:38 +0700

    Fix: Gradle error for Glide
    Fix: Base model with message attribute
    Fix: ApiCallback to handle network error exception
    Fix: observeOn and subscribOn RxJava network request
    Fix: Update kotlin
    Fix: Update gradle version
    Fix: Enable gradle parallel execution

--- app/src/main/java/id/gits/gitsmvvmkotlin/data/source/remote/ApiCallback.kt
+++ app/src/main/java/id/gits/gitsmvvmkotlin/data/source/remote/ApiCallback.kt
@@ -12,11 +12,11 @@ import java.net.UnknownHostException
  * Created by irfanirawansukirman on 26/01/18.
  */
 
-abstract class ApiCallback<M>: Observer<M> {
+abstract class ApiCallback<M> : Observer<M> {
 
     abstract fun onSuccess(model: M)
 
-    abstract fun onFailure(errorMessage: String)
+    abstract fun onFailure(code: Int, errorMessage: String)
 
     abstract fun onFinish()
 
@@ -45,24 +45,35 @@ abstract class ApiCallback<M>: Observer<M> {
                     val body = httpException.response().errorBody()
                     baseDao = Gson().fromJson<BaseApiModel<M>>(body!!.string(), BaseApiModel::class.java)
                 } catch (exception: Exception) {
-                    onFailure(exception.message!!)
+                    onFailure(code, exception.message!!)
                 }
 
-                if (code == 504) {
-                    msg = "Error Response"
+                when (code) {
+
+                    504 -> {
+                        msg = baseDao?.message ?: "Error Response"
+                    }
+                    502, 404 -> {
+                        msg = baseDao?.message ?: "Error Connect or Resource Not Found"
+                    }
+                    400 -> {
+                        msg = baseDao?.message ?: "Bad Request"
+                    }
+                    401 -> {
+                        msg = baseDao?.message ?: "Not Authorized"
+                    }
                 }
-                if (code == 502 || code == 404) {
-                    msg = "Error Connect"
-                }
+
                 if (baseDao != null) {
-//                    msg = baseDao.message
+//                    msg = baseDao.message ?: ""
                 }
-                onFailure(msg)
+
+                onFailure(code, msg)
 
             }
-            is UnknownHostException -> onFailure("An error occurred while connecting to the server")
-            is SocketTimeoutException -> onFailure("An error occurred while connecting to the server")
-            else -> onFailure(e.message!!)
+            is UnknownHostException -> onFailure(-1, "Telah terjadi kesalahan ketika koneksi ke server: ${e.message}")
+            is SocketTimeoutException -> onFailure(-1, "Telah terjadi kesalahan ketika koneksi ke server: ${e.message}")
+            else -> onFailure(-1, e.message ?: "Unknown error occured")
         }
         onFinish()
     }
